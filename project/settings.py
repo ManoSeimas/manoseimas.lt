5d319021be3a21654d48d6d95c3ad4e81044a91f
Jonas Lekevicius <jonas@lekevicius.com>
2011-09-04 18:26:36 +0300

    Setting up stylesheets and SASS workflow

--- project/settings.py
+++ project/settings.py
@@ -72,6 +72,7 @@ ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(BUILDOUT_DIR, 'project', 'static'),
+    os.path.join(BUILDOUT_DIR, 'var', 'generated_static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
