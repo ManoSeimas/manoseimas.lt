e7d7e87554860ca3e052035968ec3d9559ef59c7
sirex <sirexas@gmail.com>
2012-02-18 17:04:44 +0200

    View of Votings by legal acts.

--- manoseimas/votings/_design/views/parents/map.js
+++ manoseimas/votings/_design/views/parents/map.js
@@ -1,7 +1,14 @@
 function (doc) {
-    if (doc.doc_type == 'voting' && doc.parents) {
-        for (var i=0; i<doc.parents.length; i++) {
-            emit([doc.parents[i], doc.datetime], null);
+    if (doc.doc_type == 'Voting') {
+        if (doc.legal_acts) {
+            for (var i=0; i<doc.legal_acts.length; i++) {
+                emit([doc.legal_acts[i], doc.created], null);
+            }
+        }
+        if (doc.parent_legal_acts) {
+            for (var i=0; i<doc.parent_legal_acts.length; i++) {
+                emit([doc.parent_legal_acts[i], doc.created], null);
+            }
         }
     }
 }
