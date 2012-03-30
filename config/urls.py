e2beaa7372241d30d0d117169aa956a79a0e9d7c
sirex <sirexas@gmail.com>
2012-03-30 11:33:29 +0300

    Autodiscover all nodes in INSTALLED_APPS.

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+import sboard.factory
+
 admin.autodiscover()
+sboard.factory.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'sboard.views.node', {'action': 'list'}, name='index'),
