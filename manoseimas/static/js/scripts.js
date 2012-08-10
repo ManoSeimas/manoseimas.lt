7300f7fe23a1d4182036baa12cd32a4277a9688b
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2012-08-10 12:17:35 +0300

    Dynamically activate the current test category menu item.

--- manoseimas/static/js/scripts.js
+++ manoseimas/static/js/scripts.js
@@ -8,3 +8,55 @@ function ensureSidebarFits() {
 }
 
 $(ensureSidebarFits);
+
+function activateMenuItem(id) {
+  $('.sidebar li').removeClass('active');
+  $('.sidebar a[href="#' + id + '"]').parent().addClass('active');
+}
+
+function chooseMenuAnchor() {
+  var viewportHeight = $(window).height();
+  var viewportCenter = $(window).scrollTop() + viewportHeight / 2;
+
+  function centerDistance(elem) {
+    var elemCenter = $(elem).offset().top + Math.min($(elem).height(), viewportHeight) / 2;
+    return Math.abs(viewportCenter - elemCenter);
+  }
+
+  var minDistance = Infinity;
+  var minId;
+
+  $('h1').each(function(index, item) {
+    var d = centerDistance($(item).parent());
+    if (d < minDistance) {
+      minDistance = d;
+      minId = item.id;
+    }
+  });
+
+  if (minDistance < Infinity) {
+    activateMenuItem(minId);
+  }
+}
+
+function throttle(delay, fn) {
+  var timer = null;
+  var lastVal = null;
+  var postFire = false;
+  return function (args) {
+    if (timer) {
+      postFire = true;
+      return lastVal;
+    } else {
+      lastVal = fn.apply(args);
+      timer = setTimeout(function() {
+        if (postFire) {
+          fn.apply(args);
+        }
+        postFire = false;
+        timer = null;
+      }, delay);
+      return lastVal;
+    }
+  }
+}
