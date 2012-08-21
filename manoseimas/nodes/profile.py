85a4fdfb13666fd294431decc01811072f3454d6
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2012-08-21 19:31:06 +0300

    user profile: Update profile page to design.

--- manoseimas/nodes/profile.py
+++ manoseimas/nodes/profile.py
@@ -1,17 +1,33 @@
+# coding: utf-8
 from zope.component import adapts
 from zope.component import provideAdapter
+from django.utils.translation import ugettext as _
 from sboard.profiles.interfaces import IProfile
 from sboard.profiles.nodes import ProfileView
 from manoseimas.compat.models import PersonPosition
 from manoseimas.mps.nodes import prepare_position_list
+
+
+USER_POSITION_NAMES = {
+    -2: _('Tikrai ne'),
+    -1: _('Ne'),
+    1: _('Taip'),
+    2: _('Tikrai taip'),
+}
+
 
 class ManoseimasProfileView(ProfileView):
     adapts(IProfile)
     template = 'sboard/profile.html'
 
     def render(self, **overrides):
+        positions = [{
+            'solution': pp.node,
+            'position': USER_POSITION_NAMES[int(pp.position)],
+        } for pp in PersonPosition.objects.filter(profile=self.node)]
+        positions.sort(key=lambda p: p['solution'].ref.title)
         context = {
-            'positions': prepare_position_list(self.node),
+            'positions': positions,
         }
         context.update(overrides)
         return super(ManoseimasProfileView, self).render(**context)
