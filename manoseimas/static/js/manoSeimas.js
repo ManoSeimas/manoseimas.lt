15346f549bed300d8752efc51611e3e897bb1ce5
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2012-08-24 12:42:33 +0300

    test page: Use scrollspy.

--- manoseimas/static/js/manoSeimas.js
+++ manoseimas/static/js/manoSeimas.js
@@ -13,63 +13,6 @@
 
   $(manoSeimas.ensureSidebarFits);
 
-  function activateMenuItem(id) {
-    $('.sidebar li').removeClass('active');
-    $('.sidebar a[href="#' + id + '"]').parent().addClass('active');
-  }
-
-  function chooseMenuAnchor() {
-    var viewportHeight = Math.min($(window).height(), 600);
-    var viewportCenter = $(window).scrollTop() + viewportHeight / 2;
-
-    function centerDistance(elem) {
-      var elemCenter = $(elem).offset().top + Math.min($(elem).height(), viewportHeight) / 2;
-      return Math.abs(viewportCenter - elemCenter);
-    }
-
-    var minDistance = Infinity;
-    var minId;
-
-    $('h1').each(function(index, item) {
-      var d = centerDistance($(item).parent());
-      if (d < minDistance) {
-        minDistance = d;
-        minId = item.id;
-      }
-    });
-
-    if (minDistance < Infinity) {
-      activateMenuItem(minId);
-    }
-  }
-
-  manoSeimas.chooseMenuAnchor = chooseMenuAnchor;
-
-  function throttle(delay, fn) {
-    var timer = null;
-    var lastVal = null;
-    var postFire = false;
-    return function (args) {
-      if (timer) {
-        postFire = true;
-        return lastVal;
-      } else {
-        lastVal = fn.apply(args);
-        timer = setTimeout(function() {
-          if (postFire) {
-            fn.apply(args);
-          }
-          postFire = false;
-          timer = null;
-        }, delay);
-        return lastVal;
-      }
-    }
-  }
-
-  manoSeimas.throttle = throttle;
-
-
   function submitPosition(url, node_id, position, callback) {
     var postData = {
       node: "+" + node_id,
