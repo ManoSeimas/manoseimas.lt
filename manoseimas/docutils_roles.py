0e248eda116dd9da5626db6be67696df31352f46
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2012-08-31 12:46:26 +0300

    reST: Implement 'description' role.

---
+++ manoseimas/docutils_roles.py
@@ -0,0 +1,32 @@
+import re
+import cgi
+
+from docutils.parsers.rst import roles
+from docutils import nodes
+from docutils import utils
+
+
+# Define description role for docutils.
+DESCRIPTION_RE = re.compile(r'^(.*?) \((.*)\)$')
+def description_role(role, rawtext, text, lineno, inliner,
+                     options={}, content=[]):
+
+    match = DESCRIPTION_RE.match(text)
+    if not match:
+        msg = inliner.reporter.error(
+            'Description text should be in the format "Thing (description)"; '
+            '"%s" is invalid.' % text, line=lineno)
+        prb = inliner.problematic(rawtext, rawtext, msg)
+        return [prb], [msg]
+    thing, description = match.groups()
+    html = u'<span class="description" title="%s">%s</span>' % (
+        cgi.escape(description),
+        cgi.escape(thing),
+    )
+    options['format'] = 'html'
+    roles.set_classes(options)
+    node = nodes.raw(rawtext, html, **options)
+    node.source, node.line = inliner.reporter.get_source_and_line(lineno)
+    return [node], []
+
+roles.register_local_role('description', description_role)
