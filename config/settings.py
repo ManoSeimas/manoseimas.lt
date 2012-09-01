f05894eedbd04395ea1eb4b44e35c4a8ddba0c6b
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2012-09-01 19:16:05 +0300

    backout 456: jsmin

--- config/settings.py
+++ config/settings.py
@@ -171,7 +171,7 @@ PIPELINE_LESS_ARGUMENTS = '--compress --include-path=%s' % LESS_PATH
 #end if
 
 PIPELINE_CSS_COMPRESSOR = None
-PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.jsmin.JSMinCompressor'
+PIPELINE_JS_COMPRESSOR = None
 
 #end if
 
