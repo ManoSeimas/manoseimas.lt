0f49b60f97479357b6398df3e62f9c50e89a6807
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2012-09-05 12:11:48 +0300

    compat: Change activity tresholds to 25%

--- manoseimas/compat/models.py
+++ manoseimas/compat/models.py
@@ -76,7 +76,7 @@ PROFILE_TYPES = (
 
 
 PARTICIPATION_SHOW_TRESHOLD = 0.1
-PARTICIPATION_ACTIVE_TRESHOLD = 0.33
+PARTICIPATION_ACTIVE_TRESHOLD = 0.25
 
 
 class PersonPositionManager(models.Manager):
@@ -174,7 +174,7 @@ class PersonPosition(models.Model):
 PRECISION_SHOW_TRESHOLD = 0.1
 
 # Results with lower precision get grayed out.
-PRECISION_PRECISE_TRESHOLD = 0.33
+PRECISION_PRECISE_TRESHOLD = 0.25
 
 
 class Compatibility(object):
