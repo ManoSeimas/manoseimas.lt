3bf4198318518c0014134299ce86e8f0c808865c
Jaro <jaro@nous.lt>
2015-06-04 16:43:06 +0300

    Added new styles.

---
+++ manoseimas/static/js/semantic_initializers.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('.ui.checkbox').checkbox('toggle');
+  $('.ui.dropdown').dropdown();
+  $('.ui.indicating.progress').progress({showActivity: false});
+  $('.ui.sticky').sticky({context: '.parliamentarians'});
+  $('.tabular.menu .item').tab();
+});
