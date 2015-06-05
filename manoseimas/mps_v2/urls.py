b54c21907e674547f8848b02c02dbee680e1efa5
Jaro <jaro@nous.lt>
2015-06-05 18:18:29 +0300

    More proper ordering for routes.

--- manoseimas/mps_v2/urls.py
+++ manoseimas/mps_v2/urls.py
@@ -5,8 +5,8 @@ from manoseimas.mps_v2 import views
 
 urlpatterns = patterns('',
     url(r'^$', views.mp_list, name='mp_list_all'),
-    url(r'^(?P<fraction_slug>[-_\w]+)/$', views.mp_list, name='mp_list_fraction'),
-    url(r'^fractions$', views.mp_fraction_list, name='mp_fraction_list'),
+    url(r'^fractions/?$', views.mp_fraction_list, name='mp_fraction_list'),
     url(r'^fractions/(?P<fraction_slug>[-_\w]+)/$', views.mp_fraction, name='mp_fraction'),
     url(r'^parliamentarian/(?P<mp_slug>.+)/$', views.mp_profile, name='mp_profile'),
+    url(r'^(?P<fraction_slug>[-_\w]+)/$', views.mp_list, name='mp_list_fraction'),
 )
