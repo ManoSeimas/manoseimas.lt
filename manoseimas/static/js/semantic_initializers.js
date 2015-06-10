cd6769f7b6625d6c2857938511a3900d92adfb63
Jaro <jaro@nous.lt>
2015-06-10 19:47:37 +0300

    Css fixes for mp_catalog view.

--- manoseimas/static/js/semantic_initializers.js
+++ manoseimas/static/js/semantic_initializers.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $('.ui.checkbox').checkbox('toggle');
   $('.ui.dropdown').dropdown();
   $('.ui.indicating.progress').progress({showActivity: false});
-  $('.ui.sticky').sticky({context: '.parliamentarians'});
   $('.tabular.menu .item').tab();
   $('.rating').popup();
 ;
