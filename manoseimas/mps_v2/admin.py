8a8c37457b249576809f5306cee0fdf3d9194c85
Saulius Žemaitaitis <saulius@zemaitaitis.lt>
2015-06-15 12:10:00 +0300

    Show Votings in admin panel.

--- manoseimas/mps_v2/admin.py
+++ manoseimas/mps_v2/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from manoseimas.mps_v2.models import ParliamentMember, GroupMembership, Group
 from manoseimas.mps_v2.models import PoliticalParty
 from manoseimas.mps_v2.models import Stenogram, StenogramTopic
-from manoseimas.mps_v2.models import StenogramStatement
+from manoseimas.mps_v2.models import StenogramStatement, Voting
 from manoseimas.mps_v2.models import MPRanking, GroupRanking
 
 
@@ -15,10 +15,12 @@ class GroupAdmin(admin.ModelAdmin):
 class MpMembershipAdmin(admin.TabularInline):
     model = ParliamentMember.groups.through
     fk_name = 'member'
+    extra = 0
 
 
 class ParliamentMemberAdmin(admin.ModelAdmin):
     inlines = (MpMembershipAdmin,)
+    readonly_fields = ('slug',)
 
 
 class StenogramStatementInline(admin.TabularInline):
@@ -50,5 +52,6 @@ admin.site.register(PoliticalParty)
 admin.site.register(Stenogram, StenogramAdmin)
 admin.site.register(StenogramTopic, StenogramTopicAdmin)
 admin.site.register(StenogramStatement)
+admin.site.register(Voting)
 admin.site.register(MPRanking)
 admin.site.register(GroupRanking)
