a4efc8012a29c809a2bf3e6f9a785f469120a7c5
Saulius Žemaitaitis <saulius@zemaitaitis.lt>
2015-06-15 23:42:16 +0300

    Fix link following, nest passed docs inside project items.

--- manoseimas/scrapy/items.py
+++ manoseimas/scrapy/items.py
@@ -213,8 +213,8 @@ class ProposedLawProjectProposer(Item):
     project_name = Field()
     project_number = Field()
     project_url = Field()
-    date = Field(input_processor=processor.Identity(),
-                 output_processor=processor.TakeFirst())
+    passed = Field(input_processor=processor.Identity(),
+                   output_processor=processor.TakeFirst())
     source = Field(input_processor=processor.Identity(),
                    output_processor=processor.TakeFirst())
 
--- manoseimas/scrapy/spiders/law_projects.py
+++ manoseimas/scrapy/spiders/law_projects.py
@@ -81,7 +81,8 @@ class LawProjectSpider(ManoSeimasSpider):
     )
 
     rules = [
-        Rule(mp_projects_link_extractor, 'parse_mp_project_index'),
+        Rule(mp_projects_link_extractor, 'parse_mp_project_index',
+             follow=True),
     ]
 
     pipelines = (
@@ -113,22 +114,22 @@ class LawProjectSpider(ManoSeimasSpider):
         loader.add_value('project_number', self._extract_proposal_no(xs))
         loader.add_value('source', self._get_source(response.url, 'p_asm_id'))
         loader.add_value('project_number', self._extract_proposal_no(xs))
-        yield loader
         passed_xs = xs.xpath('td[4]/a')
         if passed_xs:
-            loader = Loader(self, item=PassedLawProjectProposer(),
+            passed = Loader(self, item=PassedLawProjectProposer(),
                             selector=passed_xs, response=response)
             doc_id = self._get_query_attr(
                 passed_xs.xpath('@href').extract()[0], 'p_id'
             )
-            loader.add_value('id', doc_id)
+            passed.add_value('id', doc_id)
             doc_number = self._extract_passed_no(passed_xs)
-            loader.add_value('passing_number', doc_number)
-            loader.add_xpath('passing_url', '@href')
-            loader.add_value('date', proposal_date)
-            loader.add_value('source', self._get_source(response.url,
+            passed.add_value('passing_number', doc_number)
+            passed.add_xpath('passing_url', '@href')
+            passed.add_value('date', proposal_date)
+            passed.add_value('source', self._get_source(response.url,
                                                         'p_asm_id'))
-            yield loader
+            loader.add_value('passed', passed.load_item())
+        yield loader
 
     def parse_mp_project_index(self, response):
         sel = Selector(response)
