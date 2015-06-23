a071a26a90829515cd22f93e86bed326e18e2913
Jaro <jaro@nous.lt>
2015-06-23 20:03:38 +0300

    Let everywhere use url with or without ‘/‘ in the end.

--- manoseimas/mps_v2/json_urls.py
+++ manoseimas/mps_v2/json_urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns(
     '',
     url(r'^fractions/?$', views.fractions_json, name='fractions_json'),
     url(r'^mps/?$', views.mps_json, name='mps_json'),
-    url(r'^law_projects/(?P<mp_slug>.+)/$',
+    url(r'^law_projects/(?P<mp_slug>.+)/?$',
         views.law_projects_json, name='law_projects_json'),
-    url(r'^discussion/(?P<statement_id>\d+)$',
+    url(r'^discussion/(?P<statement_id>\d+)/?$',
         views.mp_discussion_json, name='mp_discussion_json'),
 )
