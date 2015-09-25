ManoSeimas/manoseimas.lt

commit 27dc0b705ae3ddae3def16eeb0f440f15b6b84a3

Make 'bin/django migrate' create widget views in CouchDB
Fixes #77.

manoseimas/widget/couchdb.py
@@ -1,0 +1,21 @@
+# couchdbkit won't sync our CouchDB views because there are no document
+# schemas registered in manoseimas.widget, so we have to load the views
+# ourselves.
+
+import os
+
+from couchdbkit import push
+from sboard.models import Node
+
+
+def syncdb(app, verbosity=2):
+    # Fun fact: syncdb will never be called for manoseimas.widget.models,
+    # because we don't have any MySQL models here.  Since we want to run
+    # exactly once, let's hook up after the main app.
+    if app.__name__ != 'manoseimas.models':
+        return
+    if verbosity >= 1:
+        print "sync `manoseimas.widget` in CouchDB"
+    db = Node.get_db()
+    path = os.path.join(os.path.dirname(__file__), '_design')
+    push(path, db, force=True, docid='_design/widget')

manoseimas/widget/__init__.py
@@ -1,0 +1,9 @@
+from django.db.models import signals
+
+
+def syncdb(app, created_models, verbosity=2, **kwargs):
+    from manoseimas.widget.couchdb import syncdb
+    syncdb(app, verbosity=verbosity)
+
+
+signals.post_syncdb.connect(syncdb)

commit 4b2429d5a004f0536405a362ba7b6117ae306ca2

Fix unit tests
Broken in commit 27dc0b705ae3ddae3def16eeb0f440f15b6b84a3.  I don't
quite understand why tests accessing the test DB break if something in
that process accesses the live DB first, but anyway touching the live DB
during a unit test run is Not Nice so we won't do it.

manoseimas/settings/base.py
@@ -251,6 +251,7 @@ COUCHDB_DATABASES = (
     ('manoseimas.mps', COUCHDB_SERVER + 'nodes'),
     ('manoseimas.solutions', COUCHDB_SERVER + 'nodes'),
     ('manoseimas.votings', COUCHDB_SERVER + 'nodes'),
+    ('manoseimas.widget', COUCHDB_SERVER + 'nodes'),
 )
 
 ELASTICSEARCH_SERVERS = (

manoseimas/widget/couchdb.py
@@ -4,8 +4,9 @@
 
 import os
 
+from django.conf import settings
+from couchdbkit.ext.django.loading import CouchdbkitHandler
 from couchdbkit import push
-from sboard.models import Node
 
 
 def syncdb(app, verbosity=2):
@@ -16,6 +17,12 @@ def syncdb(app, verbosity=2):
         return
     if verbosity >= 1:
         print "sync `manoseimas.widget` in CouchDB"
-    db = Node.get_db()
+    # Getting the database is a tricky business: this gets called early
+    # during unit test setup, and we don't want to touch the real DB
+    # during unit test runs, do we?  At this point settings.COUCHDB_DATABASES
+    # has been modified by sboard.testrunner.SboardTestSuiteRunner, but
+    # nothing else.
+    handler = CouchdbkitHandler(settings.COUCHDB_DATABASES)
+    db = handler.get_db('widget')
     path = os.path.join(os.path.dirname(__file__), '_design')
     push(path, db, force=True, docid='_design/widget')

manoseimas/widget/models.py
@@ -1,0 +1,1 @@
+# needs to exist because sboard.tests.NodesTestsMixin assumes it will exist