901a9bfc84e7faf616507d05fcdd4609c2866d48
Marius Gedminas <marius@gedmin.as>
2015-09-30 08:41:00 +0300

    Also show modified_at in the admin UI
    (I think that field would be better called updated_at, but consistency.)

--- manoseimas/lobbyists/admin.py
+++ manoseimas/lobbyists/admin.py
@@ -4,7 +4,7 @@ from .models import Lobbyist
 
 
 class LobbyistAdmin(admin.ModelAdmin):
-    readonly_fields = ('slug', 'source', 'raw_data')
+    readonly_fields = ('slug', 'source', 'modified_at', 'raw_data')
 
 
 admin.site.register(Lobbyist, LobbyistAdmin)
