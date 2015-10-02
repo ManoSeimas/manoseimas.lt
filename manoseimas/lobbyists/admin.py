a976ddf4d93fc7d48937a547c296ff473d09ffbb
Marius Gedminas <marius@gedmin.as>
2015-10-02 16:59:03 +0300

    Hook up lobbyist declarations to the admin UI

--- manoseimas/lobbyists/admin.py
+++ manoseimas/lobbyists/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-from .models import Lobbyist
+from . import models
 
 
 class LobbyistAdmin(admin.ModelAdmin):
     readonly_fields = ('slug', 'source', 'modified_at', 'raw_data')
 
 
-admin.site.register(Lobbyist, LobbyistAdmin)
+admin.site.register(models.Lobbyist, LobbyistAdmin)
+admin.site.register(models.LobbyistDeclaration)
+admin.site.register(models.LobbyistClient)
+admin.site.register(models.LobbyistLawProject)
