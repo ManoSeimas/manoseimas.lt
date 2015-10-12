f80b0e08c96b0338cb6736c3c5dde4ea5ffd0b87
Laurynas Speičys <laurynas@pov.lt>
2015-10-12 15:11:01 +0300

    Note the awkward truth.

--- manoseimas/lobbyists/views/lobbyists.py
+++ manoseimas/lobbyists/views/lobbyists.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 from manoseimas.lobbyists.models import Lobbyist
 
 def lobbyist_list(request):
+    """A placeholder."""
     lobbyists = Lobbyist.objects.all()
     return render(request, 'lobbyist_list.jade', {lobbyists: lobbyists})
