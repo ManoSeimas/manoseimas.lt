f9641e5cbc6ada14b48db53f034a8a25e84e9534
Laurynas Speičys <laurynas@pov.lt>
2015-10-12 16:54:24 +0300

    WIP: add a placeholder lobbyist profile page.

--- manoseimas/lobbyists/urls.py
+++ manoseimas/lobbyists/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, patterns, url
 
+from manoseimas.lobbyists import views
+
 urlpatterns = patterns(
     '',
     url(r'^$', 'manoseimas.lobbyists.views.lobbyists.lobbyist_list'),
+    url(r'^lobbyist/(?P<lobbyist_slug>.+)/$',
+        views.lobbyist_profile, name='lobbyist_profile'),
     url(r'^json/', include('manoseimas.lobbyists.json_urls')),
 )
+ 
