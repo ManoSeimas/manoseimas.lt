b3b5de156abf8fc131581d5ab4434ebff492bc41
Laurynas Speičys <laurynas@pov.lt>
2015-10-14 14:49:02 +0300

    Dont allow slashes in slug.

--- manoseimas/lobbyists/json_urls.py
+++ manoseimas/lobbyists/json_urls.py
@@ -4,7 +4,7 @@ from manoseimas.lobbyists import views
 
 urlpatterns = patterns(
     '',
-    url(r'^lobbyists/$', views.lobbyists_json, name='lobbyists_json'),
-    url(r'^law_projects/(?P<lobbyist_slug>.+)/?$',
+    url(r'^lobbyists/?$', views.lobbyists_json, name='lobbyists_json'),
+    url(r'^law_projects/(?P<lobbyist_slug>[^/]+)/?$',
         views.law_projects_json, name='law_projects_json'),
 )
