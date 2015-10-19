ManoSeimas/manoseimas.lt

commit aab964866d9dc4579b726b5afb095ded52b126ba

Fix more names.

manoseimas/scrapy/spiders/suggestions.py
@@ -333,6 +333,7 @@ class SuggestionsSpider(ManoSeimasSpider):
         submitter = re.sub(ur' +\d+\.\d+\.\d+\.\d+', '', submitter)
         names = [
             u'Adolf(as|o)',
+            u'Agn(ė|ės)?'
             u'Aleksand(as|o)',
             u'Alg(is|io)',
             u'Antan(as|o)',
@@ -352,7 +353,7 @@ class SuggestionsSpider(ManoSeimasSpider):
             u'Dain(ius|iaus)',
             u'Daiv(a|os)',
             u'Dali(a|os)',
-            u'Danguolė(|s)',
+            u'Danguol(ė|ės)',
             u'Danguolė(s)',
             u'Dar(ius|iaus)',
             u'Deivid(as|o)',
@@ -394,10 +395,10 @@ class SuggestionsSpider(ManoSeimasSpider):
             u'Liucij(us|aus)',
             u'Leonard(|o)',
             u'Loret(a|os)',
-            u'Marij(a|os)',
+            u'ij(a|os)',
             u'Martyn(a|os)',
             u'Mečislov(as|o)',
-            u'Miglė(s)',
+            u'Migl(ė|ės)',
             u'Mild(a|os)',
             u'Mindaug(as|o)',
             u'Nomed(a|os)',
@@ -411,13 +412,14 @@ class SuggestionsSpider(ManoSeimasSpider):
             u'Petr(as|o)',
             u'Povil(as|o)',
             u'Raimund(as|o)',
-            u'Ramutė(|s)'
+            u'Ramut(ė|ės)',
             u'Ras(a|os)',
             u'Remigij(us|aus)',
-            u'Rimantė(|s)',
+            u'Rimant(ė|ės)',
             u'Rimant(as|o)',
             u'Rim(a|os)',
             u'Rit(a|os)',
+            u'Robert(as|o)',
             u'Roland(as|o)',
             u'Rok(as|o)',
             u'Saul(ius|iaus)',

commit 2bea067476a50f467b14d7f4c78f16392bd8a4a2

Fix names normalizer
- Revert the broken bit from aab9648 (Marijos -> ijos)
- Update tests to expect initialed names
- Replace whole words only to avoid "M.polės miesto savivaldybė"
- Undo the nonsensical "V. Didžiojo universitetas" construction

manoseimas/scrapy/spiders/suggestions.py
@@ -386,7 +386,7 @@ class SuggestionsSpider(ManoSeimasSpider):
         # Split reason:
         #     /python2.7/sre_compile.py
         #     AssertionError: "sorry, but this version only supports 100 named groups"
-        submitter = re.sub(u'|'.join(names), lambda m: m.group(0)[0] + '.', submitter)
+        submitter = re.sub(ur'\b(?:%s)\b' % u'|'.join(names), lambda m: m.group(0)[0] + '.', submitter)
         names = [
             u'Laim(a|os)',
             u'Laris(a|os)',
@@ -395,7 +395,7 @@ class SuggestionsSpider(ManoSeimasSpider):
             u'Liucij(us|aus)',
             u'Leonard(|o)',
             u'Loret(a|os)',
-            u'ij(a|os)',
+            u'Marij(a|os)',
             u'Martyn(a|os)',
             u'Mečislov(as|o)',
             u'Migl(ė|ės)',
@@ -441,7 +441,7 @@ class SuggestionsSpider(ManoSeimasSpider):
             u'Vytaut(as|o)',
             u'Zit(a|os)',
         ]
-        submitter = re.sub(u'|'.join(names), lambda m: m.group(0)[0] + '.', submitter)
+        submitter = re.sub(ur'\b(?:%s)\b' % u'|'.join(names), lambda m: m.group(0)[0] + '.', submitter)
         replacements = {
             u'departamenras': 'departamentas',
             u'departamentamentas': 'departamentas',
@@ -534,6 +534,7 @@ class SuggestionsSpider(ManoSeimasSpider):
             u'Lietuvos nacionalinė sveikatos tarybos': u'Lietuvos nacionalinės sveikatos tarybos',
             u'LIETUVOS GEOGRAFŲ DRAUGIJA': u'Lietuvos geografų draugija',
             u'Lietuvos Nepriklausomybės Akt': u'Lietuvos nepriklausomybės akt',
+            u'V. Didžiojo': u'Vytauto Didžiojo',  # our names normalizer is too greedy
         }
         for a, b in sorted(replacements.items()):
             submitter = submitter.replace(a, b)

manoseimas/scrapy/tests/test_suggestions.py
@@ -851,10 +851,10 @@ class TestSubmitterCleaning(unittest.TestCase):
             u"Aukščiausiasis Teismas",
         ],
         # Some people put their home addresses, phone numbers, emails or IP addresses
-        u'Antanas Mitašiūnas': [
+        u'A. Mitašiūnas': [
             u"Antanas Mitašiūnas, Kalvarijų 999-99, LT-99999 Vilnius, tel. 9-999-99999"
         ],
-        u"Juozas Vaitkus": [
+        u"J. Vaitkus": [
             u"Juozas Vaitkus el. paštu juoas.vaitkus@ff.vu.lt",
         ],
         u"Rūta": [