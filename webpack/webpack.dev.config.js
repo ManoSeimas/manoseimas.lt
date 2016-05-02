a6617112f2f4fd1ab73d09de21bd2005ce241827
Jaro <jaro@nous.lt>
2016-05-02 19:37:22 +0300

    Added post-css and css-modules support.

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,17 +1,22 @@
 var path = require('path');
 var webpack = require("webpack")
 var BundleTracker = require('webpack-bundle-tracker')
-var WatchIgnorePlugin = require('watch-ignore-webpack-plugin')
+
+var precss        = require('precss'),
+    autoprefixer  = require('autoprefixer'),
+    lost          = require('lost'),
+    postcssImport = require('postcss-import')
 
 var config = require('./webpack.base.config.js')
 var app_root = '../manoseimas'
+
 
 config.devtool = 'eval-source-map'
 
 config.entry = {
     compat_test: [
-       'webpack-dev-server/client?http://localhost:3000',
-       'webpack/hot/only-dev-server',
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/dev-server',
         app_root + '/compatibility_test/client/index'
     ],
     react: ['react', 'react-dom'],
@@ -19,13 +24,13 @@ config.entry = {
 
 // override django's STATIC_URL for webpack bundles
 config.output.publicPath = 'http://localhost:3000/client/bundles/'
+config.libraryTarget = 'umd'
 
 config.plugins = config.plugins.concat([
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),  // don't reload if there is an error
+    // new webpack.NoErrorsPlugin(),  // don't reload if there is an error
     new BundleTracker({filename: './webpack-stats.json'}),
     new webpack.optimize.CommonsChunkPlugin('react', "react.bundle.js"),
-    new WatchIgnorePlugin([path.resolve(__dirname, './node_modules')])
 ])
 
 config.module.loaders.push(
@@ -33,7 +38,19 @@ config.module.loaders.push(
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loaders: ['react-hot', 'babel'],
+    }, {
+        test:   /\.css$/,
+        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
     }
 )
+
+config.postcss = function () {
+    return [
+        postcssImport({ addDependencyTo: webpack }), // Must be first item in list
+        precss,
+        autoprefixer,
+        lost
+    ]
+}
 
 module.exports = config
