91f5474d6c55560b883ab97b6ece5f7981579412
Jaro <jaro@nous.lt>
2016-05-31 02:10:09 +0300

    Let’s use localhost in webpack config. We should support not only Vagrant setup.

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -8,13 +8,13 @@ var app_root = '../manoseimas'
 config.devtool = 'eval-source-map'
 
 config.entry.compat_test = [
-    'webpack-dev-server/client?http://192.168.33.10:3000',
+    'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/dev-server',
     app_root + '/compatibility_test/client/index'
 ]
 
 // override django's STATIC_URL for webpack bundles
-config.output.publicPath = 'http://192.168.33.10:3000/client/bundles/'
+config.output.publicPath = 'http://localhost:3000/client/bundles/'
 config.libraryTarget = 'umd'
 
 config.plugins = config.plugins.concat([
