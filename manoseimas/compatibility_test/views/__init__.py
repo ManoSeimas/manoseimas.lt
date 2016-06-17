5f96f90c49352af36a07ca3d6f4b14997dbcc358
Gediminas Paulauskas <menesis@pov.lt>
2016-06-17 19:53:39 +0300

    Add votings to topics #153

--- manoseimas/compatibility_test/views/__init__.py
+++ manoseimas/compatibility_test/views/__init__.py
@@ -22,14 +22,26 @@ def topics_all():
         arguments = topic.arguments.all().values(
             'id', 'name', 'description', 'supporting'
         )
+        topic_votings = topic.topicvoting_set.all().values(
+            'voting__id', 'voting__name', 'voting__source', 'factor',
+        )
+        votings = [
+            {
+                'id': tv['voting__id'],
+                'name': tv['voting__name'],
+                'url': tv['voting__source'],
+                'factor': tv['factor'],
+            }
+            for tv in topic_votings
+        ]
         topics.append({
             'id': topic.id,
             'name': topic.name,
             'group': topic.groups.first().name,
             'description': topic.description,
             'arguments': list(arguments),
+            'votings': votings,
         })
-        # votings
     # TODO: randomise by group #153
     return topics
 
