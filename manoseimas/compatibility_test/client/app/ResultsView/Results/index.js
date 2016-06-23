3ab212d9f1b10b9a46ebdfe9a52ac49e7dc53d98
Jaro <jaro@nous.lt>
2016-06-23 04:48:01 +0300

    First calculations of similarity #140

--- manoseimas/compatibility_test/client/app/ResultsView/Results/index.js
+++ manoseimas/compatibility_test/client/app/ResultsView/Results/index.js
@@ -14,12 +14,34 @@ function getAnswer (answer) {
     }
 }
 
+function similarity (user_answers, fraction_answers) {
+    let points = 0,
+        answers_count = 0
+
+    for (let answer_id in fraction_answers) {
+        if (user_answers[answer_id]) {
+            answers_count++
+            points += Math.abs((user_answers[answer_id] + fraction_answers[answer_id]) / 2)
+        }
+    }
+
+    return Math.round((points / answers_count)*100)
+}
+
 const Results = (props) =>
     <div>
         <h1>Results page for {props.title}</h1>
+        <h2>My votes:</h2>
         <ul>
             {props.topics.map(topic => {
                 return <li key={topic.id}>{topic.name} - {getAnswer(props.answers[topic.id])}</li>
+            })}
+        </ul>
+
+        <h2>Results by fractions:</h2>
+        <ul>
+            {props.fractions.map(fraction => {
+                return <li>{fraction.title} - {similarity(props.answers, fraction.answers)}%</li>
             })}
         </ul>
         <Link to='/'>Restart test</Link>
@@ -28,13 +50,15 @@ const Results = (props) =>
 Results.propTypes = {
   title: React.PropTypes.string,
   topics: React.PropTypes.array,
-  answers: React.PropTypes.object
+  answers: React.PropTypes.object,
+  fractions: React.PropTypes.array
 }
 
 const mapStateToProps = (state) => ({
     title: state.test_state.title,
     topics: state.test_state.topics,
-    answers: state.results.answers
+    answers: state.results.answers,
+    fractions: state.results.fractions
 })
 
 export default connect((mapStateToProps), {})(Results)
