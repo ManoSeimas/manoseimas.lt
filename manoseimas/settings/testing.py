67733d8093980035ae4d212c8bf74fde9a59d983
Mantas <sirexas@gmail.com>
2016-06-23 14:36:23 +0300

    Fix annoying database issue when running tests
    Now database name is taken from 'read_default_file' parameter which points to a
    my.cnf configuration, where database name should be defined.
    
    But this does not work when running tests for a single file, database name is
    set to 'test_', but when running all tests, then database name is set to
    'test_manoseimas'.
    
    Setting database name in settings explicitly fixes this.

--- manoseimas/settings/testing.py
+++ manoseimas/settings/testing.py
@@ -16,3 +16,8 @@ WEBPACK_LOADER.update({
 })
 
 WEBPACK_COMMAND = ['npm', 'run', 'build:hot-reload']
+
+# Database name should be taken from read_default_file, but that does not work
+# with tests. Without database name running a single test gets 'test_' as
+# database name.
+DATABASES['default']['NAME'] = 'manoseimas'
