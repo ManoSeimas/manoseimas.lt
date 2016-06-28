3948b34e67dad2f4175ebff14c84ce4103c22bd1
Mantas <sirexas@gmail.com>
2016-06-28 12:39:21 +0300

    Fix webpack production settings
    `bin/django collectstatic` takes everything from `./bundles` and places into
    `./var/www/static`, but React expects things from `./var/www/static/bundles`.
    
    This change fixes it.

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -2,7 +2,7 @@ var webpack = require("webpack")
 var BundleTracker = require('webpack-bundle-tracker')
 var config = require('./webpack.base.config.js')
 
-config.output.path = require('path').resolve('./bundles')
+config.output.path = require('path').resolve('./bundles/bundles')
 
 config.plugins = config.plugins.concat([
   new BundleTracker({filename: './webpack-stats-prod.json'}),
