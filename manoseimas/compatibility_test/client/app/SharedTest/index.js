3e174fa4c9d2a7e095e3606edb83d656f4c6d389
Jaro <jaro@nous.lt>
2016-07-14 11:32:46 +0300

    Delete hardcoded localhost:8000

--- manoseimas/compatibility_test/client/app/SharedTest/index.js
+++ manoseimas/compatibility_test/client/app/SharedTest/index.js
@@ -5,7 +5,7 @@ import StartTest from '../StartTest/StartTest'
 let {test_id, topics, title} =  window.___INITIAL_STATE__
 
 ReactDOM.render(
-    <StartTest onClickHandler={() => window.open(`http://localhost:8000/test/${test_id}/#/topic/0`, '_self')}
+    <StartTest onClickHandler={() => window.open(`/test/${test_id}/#/topic/0`, '_self')}
                amount={topics.length}
                title={title} />,
     document.getElementById('shared-test-start')
