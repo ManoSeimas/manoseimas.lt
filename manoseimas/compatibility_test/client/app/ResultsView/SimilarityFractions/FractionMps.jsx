c2f015647badac10c01df2d9b421482a58ad91a0
Jaro <jaro@nous.lt>
2016-08-06 10:22:51 +0300

    Use already calculated mp similarity rather than recalculate it again.

--- manoseimas/compatibility_test/client/app/ResultsView/SimilarityFractions/FractionMps.jsx
+++ manoseimas/compatibility_test/client/app/ResultsView/SimilarityFractions/FractionMps.jsx
@@ -3,52 +3,29 @@ import styles from '../../../styles/views/results.css'
 import { SimilarityBar } from '../../../components'
 
 
-function calculate_similarity (user_answers, mp_answers) {
-    let points = 0,
-    answers_count = 0,
-    answers = user_answers
-
-    for (let answer_id in mp_answers) {
-        if (answers[answer_id] && answers[answer_id].answer) {
-            let answer_points = Math.abs((answers[answer_id].answer + Number(mp_answers[answer_id])) / 2)
-            if (answers[answer_id].important) {
-                answers_count += 2
-                answer_points *= 2
-            } else {
-                answers_count++
-            }
-            points += answer_points
-        }
-    }
-
-    return Math.round((points / answers_count)*100)
-}
-
 const FractionMps = ({mps, user_answers, showMoreMps}) =>
     <div className={styles.members}>
         {(mps.length < 5)
             ? mps.map(mp => {
-                let similarity = calculate_similarity(user_answers, mp.answers)
                 return <div className={styles.item} key={mp.id}>
                     <div className={styles.img}>
                         <img src={mp.logo} alt={mp.name + ' logo'} />
                     </div>
                     <main>
-                        <div className={styles.title}>{mp.name}, {mp.fraction}, {similarity}%</div>
-                        <SimilarityBar similarity={similarity} slim={true} />
+                        <div className={styles.title}>{mp.name}, {mp.fraction}, {mp.similarity}%</div>
+                        <SimilarityBar similarity={mp.similarity} slim={true} />
                     </main>
                 </div>
             })
             : [0, 1, mps.length-2, mps.length-1].map(item => {
                 let mp = mps[item]
-                let similarity = calculate_similarity(user_answers, mp.answers)
                 return <div className={styles.item} key={mp.id}>
                     <div className={styles.img}>
                         <img src={mp.logo} alt={mp.name + ' logo'} />
                     </div>
                     <main>
-                        <div className={styles.title}>{mp.name}, {mp.fraction}, {similarity}%</div>
-                        <SimilarityBar similarity={similarity} slim={true} />
+                        <div className={styles.title}>{mp.name}, {mp.fraction}, {mp.similarity}%</div>
+                        <SimilarityBar similarity={mp.similarity} slim={true} />
                     </main>
                     {(item === 1)
                         ? <a className={styles.more} onClick={() => showMoreMps(mp.fraction_id)}>
