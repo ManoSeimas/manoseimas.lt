2bc97247771f3398e02ce37350d678d13bcad3a2
Jaro <jaro@nous.lt>
2016-08-19 01:13:33 +0300

    Move key into proper place.

--- manoseimas/compatibility_test/client/app/ResultsView/SimilarityMps/FractionsSelector.jsx
+++ manoseimas/compatibility_test/client/app/ResultsView/SimilarityMps/FractionsSelector.jsx
@@ -5,14 +5,13 @@ const FractionsSelector = ({fractions, selectFraction, selected_fractions}) =>
     <div className={styles['fractions-selector']}>
         {fractions.map(fraction => {
             if (fraction.members_amount > 0) {
-                return <div className={styles.logo}>
+                return <div className={styles.logo} key={'fraction-' + fraction.id}>
                     <div className={styles.img}
-                                key={'fraction-' + fraction.id}
-                                style={(selected_fractions.indexOf(fraction.id) > -1)
-                                        ? { border: '#646464 3px solid',
-                                            width: '65px',
-                                            height: '65px' }
-                                        : undefined}>
+                         style={(selected_fractions.indexOf(fraction.id) > -1)
+                                 ? { border: '#646464 3px solid',
+                                     width: '65px',
+                                     height: '65px' }
+                                 : undefined}>
                         <img src={fraction.logo}
                              alt={fraction.title + ' logo'}
                              onClick={() => selectFraction(fraction.id)} />
