a82467a54e7d27a70ac6774aebc9cf46ee32dfa8
Jaro <jaro@nous.lt>
2016-08-26 01:19:09 +0300

    Top 5 and bottom 5 in mps results tab.

---
+++ manoseimas/compatibility_test/client/app/ResultsView/SimilarityMps/OneMp.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { SimilarityBar, SimilarityWidget } from '../../../components'
+import styles from '../../../styles/views/results.css'
+
+const OneMp = ({mp, fraction, topics, user_answers, expandTopics, expanded_mp}) =>
+  <div className={styles.item} key={mp.id}>
+      <div className={styles.logo}>
+          <div className={styles.img}>
+              <img src={mp.logo} alt={mp.title + ' logo'} />
+          </div>
+          <img src={fraction.logo} className={styles['fraction-logo']} />
+      </div>
+      <main>
+          <div className={styles.title}>{mp.name}, {mp.fraction}, {mp.similarity}%</div>
+          <SimilarityBar similarity={mp.similarity} />
+          <a onClick={() => expandTopics(mp.id)}>
+              Atsakymai pagal klausimus
+              <div className={styles.arrow}></div>
+          </a>
+      </main>
+      {(expanded_mp === mp.id)
+          ? <div className={styles.topics}><ol>
+              {topics.map(topic => {
+                  return <li key={topic.id}>
+                      {topic.name} <br />
+                      <SimilarityWidget topic={topic}
+                                        items={[mp]}
+                                        user_answers={user_answers} />
+                  </li>
+              })}
+            </ol></div>
+          : ''
+      }
+  </div>
+
+export default OneMp
